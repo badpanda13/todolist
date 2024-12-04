510e5e15bf1727a14b6c79bd8dafcb99bb4238e1
Tatsiana <badpanda@tut.by>
2024-12-04 16:45:47 +0300

    Added sweet style to todo-list

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from './store/store';
 import { addTodo, toggleTodo, deleteTodo } from './store/todoSlice';
+import DeleteIcon from './assets/remove-icon.png';
 import './App.css';
 
 const App: React.FC = () => {
@@ -17,29 +18,29 @@ const App: React.FC = () => {
     };
 
     return (
-        <div className="App">
-            <h1>To-Do List</h1>
-            <input
-                type="text"
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-                placeholder="Add a new task"
-            />
-            <button onClick={handleAddTodo}>Add</button>
-            <ul>
-                {todos.map((todo) => (
-                    <li key={todo.id}>
-                        <span
-                            onClick={() => dispatch(toggleTodo(todo.id))}
-                            style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
-                        >
-                            {todo.text}
-                        </span>
-                        <button onClick={() => dispatch(deleteTodo(todo.id))}>Delete</button>
-                    </li>
-                ))}
-            </ul>
+      <div className="App">
+        <h1>To-Do List</h1>
+        <div className="todo-input">
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Add a new task"
+          />
+          <button onClick={handleAddTodo}>Add</button>
         </div>
+        <ul className="todo-list">
+          {todos.map((todo, index) => (
+            <li key={index}>
+              <input type="checkbox"  onClick={() => dispatch(toggleTodo(todo.id))}
+              className="todo-checkbox" checked={todo.completed}/>
+              <span  style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>{todo.text}</span>
+              <img src={DeleteIcon} alt="delete" className="delete-icon" 
+              onClick={() => dispatch(deleteTodo(todo.id))}/>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
 };
 
